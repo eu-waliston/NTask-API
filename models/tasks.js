951a20de7242365f9e1b62346e3752869b606e56
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-16 14:13:15 -0300

    Ntask

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,28 @@
-module.exports = app => {
-    return {
-        findAll: (params, callback) => {
-            return callback([
-                {title: "Fazer compras"},
-                {title: "Concertar o pc"}
-            ])
+module.exports = (sequelize, DataType) => {
+    const Tasks = sequelize.define("Tasks", {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        done: {
+            type: DataType.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
         }
-    }
-}
+    }, {
+        classMethods: {
+            associate: (models) => {
+                Tasks.belongsTo(models.Users);
+            }
+        }
+    });
+    return Tasks;
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,9 @@
 module.exports = app => {
-    const Tasks = app.models.tasks;
+    const Tasks = app.db.models.Tasks;
 
-    app.get("/tasks", (req, res) => {
-       Tasks.findAll({}, (tasks) => {
-        res.json({tasks: tasks})
-       });
+    app.get("/tasks", (req,res) => {
+        Tasks.findAll({}).then(tasks => {
+            res.json({tasks: tasks})
+        });
     });
-}
+};
