5f6bd29e24771b0fde2e619263203fbf2eb26f7a
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-16 12:06:49 -0300

    Ntask

--- libs/boot.js
+++ libs/boot.js
@@ -1,9 +1,7 @@
-const  Sequelize = require("sequelize") 
-
 module.exports = app => {
     
     app.listen(app.get("port"), () => {
-        Sequelize.sync({force: true})
+        
         console.log(`NTask API - porta ${app.get("port")}`);
     })
 }
