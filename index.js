0b411447e26b51ac5ac221a5e506c00735535666
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-16 11:13:44 -0300

    Ntask

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const consign = require("consign");
 
 const app = Express();
 
-consign().include("models").then("libs/middlewares.js").then("routes").then("libs/boot.js").into(app);
-
-
+consign()
+    .include("models")
+    .then("libs/middlewares.js")
+    .then("routes")
+    .then("libs/boot.js")
+    .into(app);
